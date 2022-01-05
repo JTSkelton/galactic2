4e8c4b9427d089ff2cc1e8083e08fb694119100b
Jack <skelton.jt9@gmail.com>
2022-01-04 21:27:43 -0500

    Test to calculate planet ages as added

--- _tests_/galactic.test.js
+++ _tests_/galactic.test.js
@@ -6,4 +6,13 @@ describe("Cosmic Age Test", () => {
     expect(person.name).toEqual("Jack");
     expect(person.age).toEqual(31);
   });
+
+  test("should correctly calcualte planet ages and store them in object", () => {
+    const person = new Cosmic("Jack", 31);
+    const personAges = person.convertAge();
+    expect(personAges.mercuryAge).toEqual(129);
+    expect(personAges.venusAge).toEqual(50);
+    expect(personAges.marsAge).toEqual(16);
+    expect(personAges.jupiterAge).toEqual(2);
+  });
 });
